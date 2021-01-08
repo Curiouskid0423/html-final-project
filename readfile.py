a0c6dc9289ab7ca00ab038f65a92ce856340c3ea
ImmortalCactus <huaishing@gmail.com>
2021-01-08 16:27:24 +0800

    newest

--- readfile.py
+++ readfile.py
@@ -4,14 +4,11 @@ import pandas as pd
 import re
 import time
 from datetime import date
-from sklearn.svm import SVC,SVR
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("infile")
-
+parser.add_argument("outfile")
 args=parser.parse_args()
 
 d_month = {"January":1,
@@ -52,36 +49,6 @@ label = df[['adr','is_canceled']]
 df = df.drop(['adr','is_canceled','reservation_status_date'],axis=1)
 df = df.astype(float)
 df = df.fillna(0)
+df.to_csv(args.outfile)
 
-svm = SVC(kernel='rbf',gamma = 1 ,shrinking = False)
-svm_reg = SVR(kernel ='rbf',gamma = 1,shrinking = False,C=1.0,epsilon=0.2)
-X_train,X_test,y_train,y_test=train_test_split(df,label[['is_canceled','adr']],test_size=0.4,random_state=0)
 
-print("Dadta Preprocessed")
-sc = StandardScaler()
-sc.fit(X_train)
-X_train = sc.transform(X_train)
-X_test = sc.transform(X_test)
-
-print("Normalized")
-
-svm.fit(X_train,y_train['is_canceled'].values)
-svm_reg.fit(X_train,y_train['adr'].values)
-
-print("Model Training Finished")
-
-pre = svm.predict(X_test)
-pre_adr = svm_reg.predict(X_test)
-error = 0
-error_reg_sqr = 0
-#print(pre)
-#print(pre_adr)
-#print(y_test['adr'].values)
-#print(type(pre))
-for i,v in enumerate(pre):
-    if v!=y_test['is_canceled'].values[i]:
-        error+=1
-    error_reg_sqr += pow(pre_adr[i]-y_test['adr'].values[i],2)
-    #print(str(pre[b])+" "+str(y_test['is_canceled'].values[b]))
-print(error/pre.size)
-print(error_reg_sqr/pre.size)
---
+++ svm_svr.py
@@ -0,0 +1,50 @@
+import csv
+import argparse
+import pandas as pd
+import re
+import time
+from datetime import date
+from sklearn.svm import SVC,SVR
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+parser = argparse.ArgumentParser()
+parser.add_argument("infile")
+
+args=parser.parse_args()
+
+df = pd.read_csv(args.infile, sep=r'\s*,\s*', header=0, encoding='ascii', engine='python')
+
+svm = SVC(kernel='rbf',gamma = 1 ,shrinking = False)
+svm_reg = SVR(kernel ='rbf',gamma = 1,shrinking = False,C=1.0,epsilon=0.2)
+X_train,X_test,y_train,y_test=train_test_split(df,label[['is_canceled','adr']],test_size=0.4,random_state=0)
+
+print("Dadta Preprocessed")
+sc = StandardScaler()
+sc.fit(X_train)
+X_train = sc.transform(X_train)
+X_test = sc.transform(X_test)
+
+print("Normalized")
+
+svm.fit(X_train,y_train['is_canceled'].values)
+svm_reg.fit(X_train,y_train['adr'].values)
+
+print("Model Training Finished")
+
+pre = svm.predict(X_test)
+pre_adr = svm_reg.predict(X_test)
+error = 0
+error_reg_sqr = 0
+#print(pre)
+#print(pre_adr)
+#print(y_test['adr'].values)
+#print(type(pre))
+for i,v in enumerate(pre):
+    if v!=y_test['is_canceled'].values[i]:
+        error+=1
+    error_reg_sqr += pow(pre_adr[i]-y_test['adr'].values[i],2)
+    #print(str(pre[b])+" "+str(y_test['is_canceled'].values[b]))
+print(error/pre.size)
+print(error_reg_sqr/pre.size)
