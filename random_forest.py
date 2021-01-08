6254b8b5c4920553b44319193aa9db389124a5cd
ImmortalCactus <huaishing@gmail.com>
2021-01-08 22:43:07 +0800

    uwwuwuwuwu

--- random_forest.py
+++ random_forest.py
@@ -8,27 +8,43 @@ from sklearn.model_selection import train_test_split
 parser = argparse.ArgumentParser()
 parser.add_argument("infile")
 parser.add_argument("test_data")
-parser.add_argument("outfile")
+#parser.add_argument("outfile")
 args=parser.parse_args()
 
 df = pd.read_csv(args.infile, sep=r'\s*,\s*', header=0, encoding='ascii', engine='python')
 df_test = pd.read_csv(args.test_data, sep=r'\s*,\s*', header=0, encoding='ascii', engine='python')
 
 #X_train,X_test,y_train,y_test=train_test_split(df.drop(['adr','is_canceled'],axis=1),df[['adr','is_canceled']],test_size=0.2,random_state=0)
-X_train=df.drop(['adr','is_canceled'])
+print("hello")
+X_train=df.drop(['adr','is_canceled'],axis=1)
 y_train=df[['adr','is_canceled']]
 X_test=df_test
 
-forest_class = ensemble.RandomForestClassifier(n_estimators=100)
-forest_reg = ensemble.RandomForestRegressor(200)
+missing_cols = set( X_train.columns ) - set( X_test.columns )
+for c in missing_cols:
+    X_test[c] = 0
+X_test = X_test[X_train.columns]
+print(X_train.columns)
+
+forest_class = ensemble.RandomForestClassifier(n_estimators=100,verbose=1)
+forest_reg = ensemble.RandomForestRegressor(200,verbose=1)
 
 forest_class.fit(X_train,y_train['is_canceled'])
 forest_reg.fit(X_train,y_train['adr'])
 
 predict_cancel = forest_class.predict(X_test)
 predict_adr = forest_reg.predict(X_test)
-print(predict_cancel)
-print(predict_adr)
+
+out = pd.DataFrame()
+out['stays_in_week_nights']=X_test['stays_in_week_nights']
+out['stays_in_weekend_nights']=X_test['stays_in_weekend_nights']
+out['arrival_date_year']=X_test['arrival_date_year']
+out['arrival_date_month']=X_test['arrival_date_month']
+out['arrival_date_day_of_month']=X_test['arrival_date_day_of_month']
+out['adr']=pd.Series(predict_adr)
+out['is_canceled']=pd.Series(predict_cancel)
+out.to_csv('result.csv')
+
 #accuracy = metrics.accuracy_score(y_test['is_canceled'],predict_cancel)
 #accuracy_reg = metrics.mean_squared_error(y_test['adr'],predict_adr)
 #print(accuracy)
